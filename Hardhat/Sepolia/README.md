f26112995e1020a1b9ead81425dd2b8cd3849333
gafaradetunji <gafaradetunji471@gmail.com>
2025-07-23 01:33:03 +0100

    web3Bridge Task2

---
+++ Hardhat/Sepolia/README.md
@@ -0,0 +1,23 @@
+# Sample Hardhat Project
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a Hardhat Ignition module that deploys that contract.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat ignition deploy ./ignition/modules/Storage.ts
+```
+- Storage sepolia contract address HARDHAT: 0xB35A110c9a4DF724767E4B24FABf7FfCAe1ffEA6
+
+- Storage sepolia contract address FOUNDRY: 0x7bEDB4017DF820EAe57C8C9d4a9Fb90d723d9f25
+
+- Storage coreDAO contract address FOUNDRY: 0x1480879Da40cD708A9bE3829D05BAF9350912Cc0
+
+
+https://sepolia.etherscan.io/address/0x7bedb4017df820eae57c8c9d4a9fb90d723d9f25
+
+
---
+++ Hardhat/Sepolia/hardhat.config.ts
@@ -0,0 +1,37 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
+
+const { ETHERSCAN_API_KEY, PRIVATE_KEY, SEPOLIA_URL_KEY } = process.env;
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.30",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_URL_KEY,
+      accounts: [`${PRIVATE_KEY}`],
+      chainId: 11155111,
+    },
+    coreDao: {
+      url: "https://rpc.test2.btcs.network",
+      accounts: [`${PRIVATE_KEY}`],
+      chainId: 1114,
+      gasPrice: 1000000000,
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+    customChains: [
+      {
+        network: "coreDao",
+        chainId: 1114,
+        urls: {
+          apiURL: "https://api.test2.btcs.network/api",
+          browserURL: "https://scan.test2.btcs.network",
+        },
+      },
+    ],
+  },
+};
+
+export default config;
